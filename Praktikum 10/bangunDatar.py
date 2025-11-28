62cb38edeff5fb2af5ae9ed14d0fecfaf2518d89
yulianaadwisiti-eng <yulianaadwisiti@gmail.com>
2025-11-28 22:19:24 +0700

    Update bangunDatar.py
    revisi

--- Praktikum 10/bangunDatar.py
+++ Praktikum 10/bangunDatar.py
@@ -2,19 +2,24 @@ import math
 
 def jajarGanjar(alas, tinggi):
     return alas * tinggi
+print (jajarGanjar(4,7))
 
 def persegi(sisi):
     hasil = sisi * sisi
     return hasil
+print (persegi(5))
 
 def persegi_panjang(panjang, lebar):
     hasil = panjang * lebar
     return hasil
+print (persegi_panjang(7,5))
 
 def segitiga (alas, tinggi):
     hasil = 0.5 * alas * tinggi
     return hasil
+print (segitiga(20,4))
 
 def lingkaran(jari_jari):
     hasil = math.pi * jari_jari * jari_jari
     return hasil
+print (lingkaran(7))
